cca428090c6e2dc7102d188459bd7c6a806b3de6
ABetancourt03 <80859657+ABetancourt03@users.noreply.github.com>
2021-07-02 20:25:41 -0400

    Add files via upload

--- js/btn_dark.js
+++ js/btn_dark.js
@@ -1,10 +1,18 @@
-const $btnDarkMode = document.querySelector(".btnDarkMode").addEventListener("click", btnDarkMode);
+const $btnDarkMode = document.querySelector(".btnDarkMode");
 const $body = document.getElementById("body");
+
+$btnDarkMode.addEventListener("click", btnDarkMode);
 
 function btnDarkMode(e) {
   if ($body.classList.contains("darkMode")) {
     $body.classList.remove("darkMode");
   } else {
     $body.classList.add("darkMode");
+  }
+
+  if ($body.classList.contains("darkMode")) {
+    $btnDarkMode.innerHTML = "☀️";
+  } else {
+    $btnDarkMode.innerHTML = "🌙";
   }
 }
