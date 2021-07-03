f7c8b0d06301716b76eec69498d380b85e36393b
ABetancourt03 <80859657+ABetancourt03@users.noreply.github.com>
2021-07-02 20:07:28 -0400

    Add files via upload

--- js/btn_dark.js
+++ js/btn_dark.js
@@ -1,1 +1,10 @@
-// btn_dark.js
+const $btnDarkMode = document.querySelector(".btnDarkMode").addEventListener("click", btnDarkMode);
+const $body = document.getElementById("body");
+
+function btnDarkMode(e) {
+  if ($body.classList.contains("darkMode")) {
+    $body.classList.remove("darkMode");
+  } else {
+    $body.classList.add("darkMode");
+  }
+}
