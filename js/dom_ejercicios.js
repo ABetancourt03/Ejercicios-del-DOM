955ab7b6b2d826e510ee9f94a39c99722faa8c26
ABetancourt03 <80859657+ABetancourt03@users.noreply.github.com>
2021-07-07 11:50:50 -0400

    Add files via upload

--- js/dom_ejercicios.js
+++ js/dom_ejercicios.js
@@ -1,9 +1,11 @@
 const d = document;
 const w = window;
-
+const n = navigator;
+const ua = n.userAgent;
 // Menu Hamburguesa
 const $panelBtn = d.querySelector(".panel-btn").addEventListener("click", panelBtn);
 const $panel = d.querySelector(".panel");
+d.addEventListener("scroll", cerrarMenu);
 
 function panelBtn(e) {
   if ($panel.classList.contains("panel-on")) {
@@ -11,6 +13,10 @@ function panelBtn(e) {
   } else {
     $panel.classList.add("panel-on");
   }
+}
+
+function cerrarMenu() {
+  $panel.classList.remove("panel-on");
 }
 
 // Eventos del Teclado
@@ -266,4 +272,45 @@ function responsiveTester(form) {
 responsiveTester("responsive-tester");
 
 // Deteccion de Dispositivos (User Agent)
-function userDeviceInfo(id) {}
+function userDeviceInfo(id) {
+  const $id = d.getElementById(id),
+    isMobile = {
+      android: () => ua.match(/android/i),
+      ios: () => ua.match(/iphone|ipad|ipod/i),
+      windows: () => ua.match(/windows phone/i),
+      any: function () {
+        return this.android() || this.ios() || this.windows();
+      },
+    },
+    isDesktop = {
+      linux: () => ua.match(/linux/i),
+      mac: () => ua.match(/mac/i),
+      windows: () => ua.match(/windows nt/i),
+      any: function () {
+        return this.linux() || this.mac() || this.windows();
+      },
+    },
+    isBrowser = {
+      chrome: () => ua.match(/chrome/i),
+      safari: () => ua.match(/safari/i),
+      firefox: () => ua.match(/firefox/i),
+      opera: () => ua.match(/opera/i),
+      ie: () => ua.match(/msie|iemobile/i),
+      edge: () => ua.match(/edge/i),
+      any: function () {
+        return (
+          this.chrome() || this.safari() || this.firefox() || this.opera() || this.ie() || this.edge
+        );
+      },
+    };
+
+  $id.innerHTML = `
+    <ul>
+      <li>User Agent: <b>${ua}</b></li>
+      <li>Plataforma: <b>${isMobile.any() ? isMobile.any() : isDesktop.any()}</b></li>
+      <li>Navegador: <b>${isBrowser.any()}</b></li>
+    </ul>
+    `;
+}
+
+userDeviceInfo("user-device");
