64c117d41d3efb8d611bef16bfd468b6999a82e9
ABetancourt03 <80859657+ABetancourt03@users.noreply.github.com>
2021-07-11 11:48:22 -0400

    Add files via upload

--- js/dom_ejercicios.js
+++ js/dom_ejercicios.js
@@ -489,3 +489,30 @@ function scrollSpy() {
 
 scrollSpy();
 
+// Video Inteligente
+function smartVideo() {
+  const $videos = d.querySelectorAll("video[data-smart-video]");
+  
+  const cb = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+	entry.target.play();	
+      } else {
+	entry.target.pause();
+      }
+
+      w.addEventListener("visibilitychange", (e) =>
+	d.visibilityState === "visible"
+	  ? entry.target.play()
+	  : entry.target.pause()
+      );
+    });
+  }
+
+  const observer = new IntersectionObserver(cb, {threshold: 0.5});
+
+  $videos.forEach(el => observer.observe(el));
+}
+
+smartVideo();
+
