a4e5cb0098a60997fe754b5a1e6b66ecb4497634
ABetancourt03 <80859657+ABetancourt03@users.noreply.github.com>
2021-07-12 10:30:20 -0400

    Add files via upload

--- js/dom_ejercicios.js
+++ js/dom_ejercicios.js
@@ -565,3 +565,46 @@ function contacFormValidations() {
 }
 
 contacFormValidations();
+
+// Narrador (Lector de voz)
+function speechReader() {
+  const $speechSelect = d.getElementById("speech-select"),
+    $speechTextarea = d.getElementById("speech-text"),
+    $speechBtn = d.getElementById("speech-btn");
+
+  const speechMessage = new SpeechSynthesisUtterance();
+
+  let voices = [];
+
+  d.addEventListener("DOMContentLoaded", (e) => {
+
+
+    speechSynthesis.addEventListener("voiceschanged", (e) => {
+      voices = speechSynthesis.getVoices();
+
+      voices.forEach((voice) => {
+        const $option = d.createElement("option");
+        $option.value = voice.name;
+        $option.textContent = `${voice.name} - ${voice.lang}`;
+
+        $speechSelect.appendChild($option);
+      });
+    });
+  });
+
+  d.addEventListener("change", (e) => {
+    if (e.target === $speechSelect) {
+      speechMessage.voice = voices.find(voice => voice.name === e.target.value);
+    }
+  });
+
+  d.addEventListener("click", (e) => {
+    if (e.target === $speechBtn) {
+      speechMessage.text = $speechTextarea.value;
+      speechSynthesis.speak(speechMessage);
+    }
+  });
+
+}
+
+speechReader();
